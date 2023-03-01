f2a1f4e0e6c57ba01d36df8ce35e8e3cf5c198bb
AminShK <aminshk4@gmail.com>
2023-03-01 19:57:23 +0330

    FirstProblemEndVersion

--- javaScripts.js
+++ javaScripts.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const products =[ 
+    {name:"product 1" , price: 3000},
+    {name:"product 2" , price:4000 },
+    {name:"product 3" , price:6000 },
+    {name:"product 4" , price:8000 }
+]
+
+const greaterThan = products.filter((a) => {
+    return a.price <= 5000;
+})
+
 function myFunction (a) {
     let b = new Array;
     a.forEach((item) =>{
@@ -9,4 +20,6 @@ function myFunction (a) {
     return b;
 }
 
-console.log(myFunction([ {name:"product 1" , price: 3000}, {name:"product 2" , price:4000 }, {name:"product 3" , price:6000 } ]));
+
+
+console.log(myFunction(greaterThan));
