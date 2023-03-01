4c1cd75ddc89a62c8ea6a80f580c289409511642
AminShK <aminshk4@gmail.com>
2023-03-01 15:07:14 +0330

    FirstProblemUpgraded

--- javaScripts.js
+++ javaScripts.js
@@ -1,12 +1,12 @@
 'use strict';
+
 function myFunction (a) {
-    let c = new Array;
-    for (let i = 0; i < a.length; i++) {
-        let obj = new Array;
-        obj = a[i];
-        c[i] = [obj.name,obj.price];
-    }
-    return c;
+    let b = new Array;
+    a.forEach((item) =>{
+        let c = [item.name,item.price]; 
+        b = b.concat([c]);
+    } )
+    return b;
 }
 
-console.log(myFunction([ {name:"product 1" , price: 3000}, {name:"product 2" , price:4000 } ]));
+console.log(myFunction([ {name:"product 1" , price: 3000}, {name:"product 2" , price:4000 }, {name:"product 3" , price:6000 } ]));
